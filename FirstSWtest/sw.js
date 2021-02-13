17f592d74955d6d40902fb5a60929e988c112a09
Smitesh Das <57407736+smiteshdas@users.noreply.github.com>
2021-02-13 15:00:48 +0530

    Update sw.js

--- FirstSWtest/sw.js
+++ FirstSWtest/sw.js
@@ -1,11 +1,4 @@
- importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
 
-workbox.routing.registerRoute(
-  ({request}) => request.destination === 'image',
-  new workbox.strategies.CacheFirst()
-);
-
-/*
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
   '/',
@@ -23,4 +16,4 @@ self.addEventListener('install', function(event) {
       })
   );
 });
-*/
+
