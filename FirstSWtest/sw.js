fda7aa0790fbadc9482080abc89c28b65e3da0ba
Smitesh Das <57407736+smiteshdas@users.noreply.github.com>
2021-02-13 13:14:09 +0530

    Update sw.js

--- FirstSWtest/sw.js
+++ FirstSWtest/sw.js
@@ -1,7 +1,23 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
+/* importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
 
-// This will work!
 workbox.routing.registerRoute(
   ({request}) => request.destination === 'image',
   new workbox.strategies.NetworkFirst()
 );
+*/
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('fox-store').then((cache) => cache.addAll([
+      'index.html'
+      
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
